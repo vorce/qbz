b0a64b9caaf7b671bbbbfd412edfa96a97ab58ca
vicrodh <103399283+vicrodh@users.noreply.github.com>
2026-01-09 13:56:10 -0600

    feat: add tauri-plugin-dialog for file picker

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -90,6 +90,7 @@ pub fn run() {
 
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_dialog::init())
         .manage(AppState::new())
         .manage(library_state)
         .invoke_handler(tauri::generate_handler![
