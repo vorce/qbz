38427177f22ff25d3760db47b733998f418dc267
vicrodh <103399283+vicrodh@users.noreply.github.com>
2026-01-25 03:15:18 -0600

    fix(credits): reduce info icon size to match heart glyph

--- src/lib/components/views/AlbumDetailView.svelte
+++ src/lib/components/views/AlbumDetailView.svelte
@@ -357,7 +357,7 @@
             onclick={onShowAlbumCredits}
             title="Album credits"
           >
-            <svg width="18" height="18" viewBox="0 0 8 8" fill="currentColor">
+            <svg width="15" height="15" viewBox="0 0 8 8" fill="currentColor">
               <path d="M3 0c-.55 0-1 .45-1 1s.45 1 1 1 1-.45 1-1-.45-1-1-1zm-1.5 2.5c-.83 0-1.5.67-1.5 1.5h1c0-.28.22-.5.5-.5s.5.22.5.5-1 1.64-1 2.5c0 .86.67 1.5 1.5 1.5s1.5-.67 1.5-1.5h-1c0 .28-.22.5-.5.5s-.5-.22-.5-.5c0-.36 1-1.84 1-2.5 0-.81-.67-1.5-1.5-1.5z" transform="translate(2)" />
             </svg>
           </button>
